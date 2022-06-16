a6194231fe5bd2a705a20a8a63349cbeda49657b
Dener Vieira <deneribeiro10@hotmail.com>
2022-06-16 17:07:43 -0300

    add stickers

--- client/gui/homepage.py
+++ client/gui/homepage.py
@@ -1,8 +1,14 @@
 from tkinter import *
+from gui.sticker import StickerFrame
 
 class HomepageFrame(Frame):
     def __init__(self, window: Tk):
         super().__init__(window)
         self.grid(row=0, column=0, sticky="nsew")
-        self.label = Label(self, text="Hello Homepage")
-        self.label.pack()
+
+        StickerFrame(self).pack(side="left", padx=10)
+        StickerFrame(self).pack(side="left", padx=10)
+        StickerFrame(self).pack(side="left", padx=10)
+        StickerFrame(self).pack(side="left", padx=10)
+        StickerFrame(self).pack(side="left", padx=10)
+        StickerFrame(self).pack(side="left", padx=10)
